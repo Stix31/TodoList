2b642c3202cb2ffdeb523e3fe04bd71b7051c590
Thomas <thomas.washbrook@univ-tlse3.fr>
2021-12-06 15:31:06 +0100

    user story 1 : List my TODOs

--- TodoList/src/app/app.component.ts
+++ TodoList/src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoList';
+  data = {
+      "todoList" : [
+        {
+          "position": 0,
+          "status": "Open",
+          "description": "To finish my TodoList"
+      }, {
+          "position": 1,
+          "status": "Open",
+          "description": "Get the bred ths evening"
+      }, {
+          "position": 2,
+          "status": "Closed",
+          "description": "Validate the meeting"
+      },
+    ]
+  };
+
+  /**
+   * Change status of the element.
+   */
+  public changeStatus(position: number, event: { target: { checked: any; }; }) {
+    if (event.target.checked) {
+      let finnishedTodo = this.data.todoList[position];
+      finnishedTodo.status = "Closed";
+      this.data.todoList.splice(position, 1);
+      this.data.todoList.push(finnishedTodo);
+    } else {
+      let unfinnishedTodo = this.data.todoList[position];
+      unfinnishedTodo.status = "Open";
+      this.data.todoList.splice(position, 1);
+      let inserIndex = this.data.todoList.findIndex((elem) => elem.status === "Closed");
+      console.log(inserIndex);
+      this.data.todoList.splice(inserIndex, 0, unfinnishedTodo);
+    }
+    let pos = 0;
+    this.data.todoList.forEach((todo) => todo.position = pos++);
+  }
+
 }
